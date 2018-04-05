f8c575f315b560ae9f18ca77fd6f3aa74cb0bd65
Jordan Guy <jordan.guy.277@gmail.com>
2018-04-05 12:37:52 -0400

    buttons and display added

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -2,10 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      display: '0'
+    }
+  }
     render() {
       return (
         <div>
-          
+          <div className='header'>
+            <h1>Calculator</h1>
+            <h3>Elite Level Online Problem Solving</h3>
+          </div>
+          <div className='calulator'>
+            <div className='display'>
+                <p>{this.state.display}</p>
+            </div>
+            <div className='numberButtons'>
+              <button className='digit'>1</button>
+              <button className='digit'>2</button>
+              <button className='digit'>3</button>
+              <button className='digit'>4</button>
+              <button className='digit'>5</button>
+              <button className='digit'>6</button>
+              <button className='digit'>7</button>
+              <button className='digit'>8</button>
+              <button className='digit'>9</button>
+              <button className='digit'>0</button>
+            </div>
+            <div className='operatorButtons'>
+              <button className='operator'>+</button>
+              <button className='operator'>_</button>
+              <button className='operator'>/</button>
+              <button className='operator'>*</button>
+              <button className='operator'>EXP</button>
+              <button className='operator'>SQ</button>
+              <button className='operator'>DEL</button>
+            </div>
+          </div>
         </div>
       )
     }
