9f318093f829a78c6c7c14ddbcd80e0de894f24b
Jordan Guy <jordan.guy.277@gmail.com>
2018-04-09 12:29:38 -0400

    started styling

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -28,7 +28,9 @@ class App extends React.Component {
 
   clearNumbers() {
     this.setState ({
-      display: '0'
+      display: '0',
+      numberTotal: null,
+      operator: null
     })
   }
 
@@ -83,39 +85,43 @@ class App extends React.Component {
 
     render() {
       return (
-        <div>
+        <div className='wrapper'>
           <div className='header'>
-            <h1>Calculator</h1>
-            <h3>Elite Level Online Problem Solving</h3>
+            <h1>React Calculator</h1>
           </div>
           <div className='calculator'>
             <div className='display'>
                 <p>{this.state.numberTotal}{this.state.operator}</p>
                 <p>{this.state.display}</p>
             </div>
-            <div className='numberButtons'>
-              <button className='digit' onClick= {() => this.digitValue(1)}>1</button>
-              <button className='digit' onClick= {() => this.digitValue(2)}>2</button>
-              <button className='digit' onClick= {() => this.digitValue(3)}>3</button>
-              <button className='digit' onClick= {() => this.digitValue(4)}>4</button>
-              <button className='digit' onClick= {() => this.digitValue(5)}>5</button>
-              <button className='digit' onClick= {() => this.digitValue(6)}>6</button>
-              <button className='digit' onClick= {() => this.digitValue(7)}>7</button>
-              <button className='digit' onClick= {() => this.digitValue(8)}>8</button>
-              <button className='digit' onClick= {() => this.digitValue(9)}>9</button>
-              <button className='digit' onClick= {() => this.digitValue(0)}>0</button>
-              <button className='digit' onClick={() => this.inputDecimal()}>.</button>
-            </div>
-            <div className='operatorButtons'>
-              <button className='operator' onClick={() => this.executeEquation('+')}>+</button>
-              <button className='operator' onClick={() => this.executeEquation('-')}>-</button>
-              <button className='operator' onClick={() => this.executeEquation('/')}>/</button>
-              <button className='operator' onClick={() => this.executeEquation('*')}>*</button>
-              <button className="operator" onClick={() => this.executeEquation('=')}>=</button>
-              <button className='operator' onClick={() => this.executeEquation('^')}>EXP</button>
-              <button className='operator' onClick={() => this.executeEquation('√')}>SQ</button>
-              <button className='operator' onClick={() => this.clearNumbers()}>CLEAR</button>
-              <button className='operator'>DEL</button>
+            <div className='buttonContainer'>
+              <div className='numberButtons'>
+                <button className='digit' onClick= {() => this.digitValue(1)}>1</button>
+                <button className='digit' onClick= {() => this.digitValue(2)}>2</button>
+                <button className='digit' onClick= {() => this.digitValue(3)}>3</button>
+                <button className='digit' onClick= {() => this.digitValue(4)}>4</button>
+                <button className='digit' onClick= {() => this.digitValue(5)}>5</button>
+                <button className='digit' onClick= {() => this.digitValue(6)}>6</button>
+                <button className='digit' onClick= {() => this.digitValue(7)}>7</button>
+                <button className='digit' onClick= {() => this.digitValue(8)}>8</button>
+                <button className='digit' onClick= {() => this.digitValue(9)}>9</button>
+                <button className='digit' onClick= {() => this.digitValue(0)}>0</button>
+                <button className='digit' onClick={() => this.inputDecimal()}>.</button>
+                <button className="operator" onClick={() => this.executeEquation('=')}>=</button>
+              </div>
+              <div className='operatorButtons'>
+                <div className='equationButtons'>
+                  <button className='operator' onClick={() => this.executeEquation('+')}>+</button>
+                  <button className='operator' onClick={() => this.executeEquation('-')}>-</button>
+                  <button className='operator' onClick={() => this.executeEquation('/')}>/</button>
+                  <button className='operator' onClick={() => this.executeEquation('*')}>*</button>
+                </div>
+                <div className='advancedButtons'>
+                  <button className='operator' onClick={() => this.executeEquation('^')}>EXP</button>
+                  <button className='operator' onClick={() => this.executeEquation('√')}>SQ</button>
+                  <button className='operator' onClick={() => this.clearNumbers()}>CLEAR</button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
